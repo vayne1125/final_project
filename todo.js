0081bd2c43594f21226c44adfe61f5f4548aeaaf
vayne1125 <95048697+vayne1125@users.noreply.github.com>
2021-12-31 02:13:23 +0800

    Update todo.js

--- todo.js
+++ todo.js
@@ -6,14 +6,46 @@ function image_repos(){
     $(document).ready(function(){
         $("#roof").css({
             "left" : ""+wid/3+"px",
-            "top"  : ""+hei/3+"px",
+            "top"  : ""+hei/4+"px",
         });
         $("#house").css({
             "left" : ""+(wid/3+50)+"px",
-            "top"  : ""+(hei/3+100)+"px",
+            "top"  : ""+(hei/4+100)+"px",
         })
-        console.log($("#roof").attr("style"));
-        console.log($("#house").attr("style"));
+        $("#apple_with_board").css({
+            "left" : ""+(wid/3-140)+"px",
+            "top"  : ""+(hei/4+80)+"px",
+        })
+        $("#restaurant").css({
+            "left" : ""+(wid/3-80)+"px",
+            "top"  : ""+(hei/4+50)+"px",
+        })
+        $("#door_out").css({
+            "left" : ""+(wid/3+173.5)+"px",
+            "top"  : ""+(hei/4+210)+"px",
+        })
+        $("#door_in").css({
+            "left" : ""+(wid/3+173.5)+"px",
+            "top"  : ""+(hei/4+210)+"px",
+        })
+        $("#chimney").css({
+            "left" : ""+(wid/3+270)+"px",
+            "top"  : ""+(hei/4)+"px",
+        })
+        
+        $("#cloud").css({
+            "left" : ""+(wid/3+270)+"px",
+            "top"  : ""+(hei/4)+"px",
+        })
+        
+        $("checkin").css({
+            "left" : ""+(wid/3+270)+"px",
+            "top"  : ""+(hei/4)+"px",
+        })
+        $("form").css({
+            "left" : ""+(wid/3+60)+"px",
+            "top"  : ""+(hei/4+130)+"px",
+        })
     });
     doani();
 }
@@ -24,44 +56,43 @@ function doani(){
     $("#roof").addClass("roof_")
     setTimeout(function(){
         $("#house").addClass("house_")
-    },3000)
-    /*
+    },1000)
     setTimeout(function(){
         $("#apple_with_board").fadeTo(2500,1);
         $("#restaurant").fadeTo(3000,1);
-    },5000)
+    },3000)
     setTimeout(function(){
         $("#door_out").fadeIn();
         $("#door_in").fadeIn();
         $("#chimney").fadeIn();
-    },4500)
+    },1000)
     $("#door_out").mouseover(function(){
         $("#door_out").css({
             "transform":"skewY(20deg)",
-            "top":"+=10px"
+            "top"  : "+=11px",
         });
     })
     $("#door_out").mouseout(function(){
         $("#door_out").css({
             "transform":"skewY(0deg)",
-            "top":"384px",
-            "right": "615px",
+            "left" : ""+(wid/3+173.5)+"px",
+            "top"  : ""+(hei/4+210)+"px",
         });
     })
-    
+    /*
     $("#chimney").mouseover(function(){
-        $("#cloud").show();
         $("#cloud").animate({
-            "top":"50px",
-        },2000);   
+            top:"-=150px",
+        },1000)
+       
     });
     $("#chimney").mouseout(function(){
-        $("#cloud").css({
-            "top":"100px",
-            "opacity":"100"
-        }) 
+        $("#cloud").animate({
+            top:"+=150px",
+        },1000) 
+      
     });
-    
+    */
     $("#door_out").click(function(){
         $("div div").fadeTo(100,0);
         $("div div").hide();
@@ -70,5 +101,4 @@ function doani(){
             "top":"-=52px",
         })
     })
-     */
 }
