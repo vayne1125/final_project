ce0357a04558e59af21ab93d919da09a7546bfa3
mmienmien <95028987+mmienmien@users.noreply.github.com>
2022-01-13 21:37:42 +0800

    Update img_process.js

--- img_process.js
+++ img_process.js
@@ -7,11 +7,16 @@ function img_resize() {
    $(document).ready(function(){
      $("*").attr("draggable",false);
          $(".sp").attr("draggable",true);
+    
      $("#xp").css("width",""+wid+"px").css("top",""+wid/2.19+"px").css("height",""+wid+"px");
+     //$("#orinbar").fadeOut(60000);
+     $(".bar").css("width",""+wid/3+"px");
+     $("#orinbar").css("background-color","#ff7f50").animate({width:"0px"},60000);
      $(".guest").css("width",""+wid/6+"px");        //訪客
      $(".blank").css("width",""+wid/6+"px").css("height","1px");
      $(".g").on("dragover",function(event){ event.preventDefault();});
      $(".sky").on("dragover",function(event){ event.preventDefault();}); ///
+     $("#miss").css("left",""+wid/8+"px").css("top",""+wid/20+"px").css("width",""+wid/4.8+"px").hide();
      $(".head_img").css("width",""+wid/7+"px");
     $(".guest_0").css("top",""+wid/11+"px").css("left",""+wid/6+"px");
 
@@ -24,11 +29,15 @@ function img_resize() {
             $(".tom_blanket").css("margin-top","-"+wid/23+"px").css("margin-left","-"+wid/25+"px").css("width",""+wid/12+"px").css("width",""+wid/7+"px");
             $("#tom_blanket").css("margin-top",""+wid/10000+"px");
         $("#noodle").css("top",""+wid/3.1+"px").css("right",""+wid/20+"px");
+        $("#fri").css("top",""+wid/3.5+"px").css("right",""+wid/5+"px");
+        $("#chi").css("top",""+wid/2.9+"px").css("right",""+wid/3.3+"px");
         
     $(".sp").css("width",""+wid/12+"px").css("margin-right","-"+wid/26+"px").css("top","-"+wid/37+"px");
     for(let i=0;i<5;i++)$("#egg"+i).css("margin-right","-"+wid/27+"px").css("width",""+wid/36+"px");
     for(let i=0;i<5;i++)$("#tom"+i).css("margin-right","-"+wid/30+"px").css("width",""+wid/26+"px");
-    for(let i=0;i<4;i++)$("#noodle"+i).css("margin-right","-"+wid/30+"px").css("width",""+wid/20+"px");
+    for(let i=0;i<5;i++)$("#noodle"+i).css("margin-right","-"+wid/30+"px").css("width",""+wid/20+"px");
+    for(let i=0;i<5;i++)$("#fri"+i).css("margin-right","-"+wid/30+"px").css("width",""+wid/20+"px");
+    for(let i=0;i<5;i++)$("#chi"+i).css("margin-right","-"+wid/30+"px").css("width",""+wid/20+"px");
     //    $("#tomb").css("width",""+wid/32+"px");
     //
     
@@ -42,9 +51,15 @@ function img_resize() {
         for(let i=0;i<5;i++){
             $("#tom"+i).mouseover(function(){$(this).css("width",""+wid/24+"px");});   //
             $("#tom"+i).mouseout(function(){$(this).css("width",""+wid/26+"px");});}
-            for(let i=0;i<4;i++){
+            for(let i=0;i<5;i++){
                 $("#noodle"+i).mouseover(function(){$(this).css("width",""+wid/18+"px");});   //
                 $("#noodle"+i).mouseout(function(){$(this).css("width",""+wid/20+"px");});}
+            for(let i=0;i<5;i++){
+                 $("#fri"+i).mouseover(function(){$(this).css("width",""+wid/18+"px");});   //
+                $("#fri"+i).mouseout(function(){$(this).css("width",""+wid/20+"px");});}
+                for(let i=0;i<5;i++){
+                    $("#chi"+i).mouseover(function(){$(this).css("width",""+wid/18+"px");});   //
+                   $("#chi"+i).mouseout(function(){$(this).css("width",""+wid/20+"px");});}
      
  });
     sky.style.width=""+wid+"px";
@@ -56,4 +71,3 @@ function img_resize() {
 
 
     }
-
